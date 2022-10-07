2e997e399145571d710fbdf796e614a0f3004913
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-07 20:00:20 +0700

    Add: openzeppelin contracts

--- src/Cinema.sol
+++ src/Cinema.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.9;
+
+import "@openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Cinema {}
