2607addf520d04625e38320e55ad001dfc601bea
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-19 16:54:34 +0700

    Add: mint function, ticketDetails mapping

--- src/TIcket.sol
+++ src/TIcket.sol
@@ -6,8 +6,8 @@ import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract Ticket is ERC721ABurnable, Ownable {
     struct TicketDetails {
-        uint256 region;
-        uint32 cinema;
+        uint32 region;
+        uint8 cinema;
         uint8 studio;
         uint32 movie;
         uint64 time;
@@ -16,9 +16,44 @@ contract Ticket is ERC721ABurnable, Ownable {
     uint64 public constant TICKET_PRICE_WEEKDAYS = 0.001 ether;
     uint64 public constant TICKET_PRICE_WEEKEND = 0.0012 ether;
 
-    constructor() ERC721A("Cinema 21", "C21") {}
+    mapping(uint256 => mapping(uint256 => TicketDetails))
+        public cinemaToTicketDetails;
+    mapping(uint256 => uint256) public cinemaToTicketsSold;
 
-    function mintTicket() external {}
+    constructor() ERC721A("Cinema 21 Tickets", "C21") {}
+
+    function mintTicket(
+        uint256 _region,
+        uint256 _cinema,
+        uint256 _studio,
+        uint256 _movie,
+        uint256 _day,
+        uint256 _showTime,
+        uint256 _quantity
+    ) external payable {
+        uint256 price = getPrice(_day);
+        require(msg.value == price, "Wrong eth value sent");
+        uint256 ticketSold = cinemaToTicketsSold[_cinema];
+        TicketDetails storage details = cinemaToTicketDetails[_cinema][
+            ticketSold
+        ];
+        details.cinema = uint8(_cinema);
+        details.movie = uint32(_movie);
+        details.studio = uint8(_studio);
+        details.region = uint32(_region);
+        details.time = uint64(_showTime);
+        _mint(msg.sender, _quantity);
+    }
+
+    function getPrice(uint256 _day) public pure returns (uint256 price) {
+        if (_day > 5) {
+            price = TICKET_PRICE_WEEKEND;
+        } else {
+            price = TICKET_PRICE_WEEKDAYS;
+        }
+    }
 
     function burnTicket() external {}
+
+    receive() external payable {}
 }
