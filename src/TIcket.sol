d8c89f93b3084523f269d6f97d2c404640e394fb
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-19 23:11:25 +0700

    Add: checkSeats & getAvailablesSeats function

--- src/TIcket.sol
+++ src/TIcket.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "../lib/erc721a/contracts/extensions/ERC721ABurnable.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "./ICinema.sol";
 
 contract Ticket is ERC721ABurnable, Ownable {
     struct TicketDetails {
@@ -13,14 +14,21 @@ contract Ticket is ERC721ABurnable, Ownable {
         uint64 time;
     }
 
+    ICinema public cinemaInterface;
     uint64 public constant TICKET_PRICE_WEEKDAYS = 0.001 ether;
     uint64 public constant TICKET_PRICE_WEEKEND = 0.0012 ether;
 
     mapping(uint256 => mapping(uint256 => TicketDetails))
         public cinemaToTicketDetails;
     mapping(uint256 => uint256) public cinemaToTicketsSold;
+    mapping(uint256 => mapping(uint256 => mapping(uint256 => mapping(uint256 => bool))))
+        public seatStatus;
 
     constructor() ERC721A("Cinema 21 Tickets", "C21") {}
+
+    function setInterfaces(address _cinemaContract) external onlyOwner {
+        cinemaInterface = ICinema(_cinemaContract);
+    }
 
     function mintTicket(
         uint256 _region,
@@ -29,9 +37,16 @@ contract Ticket is ERC721ABurnable, Ownable {
         uint256 _movie,
         uint256 _day,
         uint256 _showTime,
-        uint256 _quantity
+        uint256[] calldata _seatNumbers
     ) external payable {
         uint256 price = getPrice(_day);
+        bool isSeatsAvailable = checkSeats(
+            _cinema,
+            _studio,
+            _showTime,
+            _seatNumbers
+        );
+        require(isSeatsAvailable, "Seat taken");
         require(msg.value == price, "Wrong eth value sent");
         uint256 ticketSold = cinemaToTicketsSold[_cinema];
         TicketDetails storage details = cinemaToTicketDetails[_cinema][
@@ -42,7 +57,7 @@ contract Ticket is ERC721ABurnable, Ownable {
         details.studio = uint8(_studio);
         details.region = uint32(_region);
         details.time = uint64(_showTime);
-        _mint(msg.sender, _quantity);
+        _mint(msg.sender, _seatNumbers.length);
     }
 
     function getPrice(uint256 _day) public pure returns (uint256 price) {
@@ -50,6 +65,50 @@ contract Ticket is ERC721ABurnable, Ownable {
             price = TICKET_PRICE_WEEKEND;
         } else {
             price = TICKET_PRICE_WEEKDAYS;
+        }
+    }
+
+    function checkSeats(
+        uint256 _cinema,
+        uint256 _studio,
+        uint256 _showTime,
+        uint256[] calldata _seats
+    ) internal view returns (bool status) {
+        uint256 seatsIndex = 0;
+        uint256[] memory seatsAvailable = getAvailableSeats(
+            _cinema,
+            _studio,
+            _showTime
+        );
+        for (uint256 i; i < seatsAvailable.length; ++i) {
+            uint256 seatAvailable = seatsAvailable[i];
+            if (_seats[seatsIndex] == seatAvailable) {
+                status = true;
+                seatsIndex++;
+            } else {
+                status = false;
+                break;
+            }
+        }
+    }
+
+    function getAvailableSeats(
+        uint256 _cinema,
+        uint256 _studio,
+        uint256 _showTime
+    ) public view returns (uint256[] memory seats) {
+        uint256 seatsIndex = 0;
+        uint256 seatsAmount = cinemaInterface.getStudioCapacity(
+            _cinema,
+            _studio
+        );
+        seats = new uint256[](seatsAmount);
+        for (uint256 i; i < seatsAmount; ++i) {
+            bool status = seatStatus[_cinema][_studio][_showTime][i + 1];
+            if (status != true) {
+                seats[seatsIndex] = i + 1;
+                seatsIndex++;
+            }
         }
     }
 
