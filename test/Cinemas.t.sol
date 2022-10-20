f78ee680259be3f357e1449060bf502baa5af37f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-20 23:50:29 +0700

    Add: add region,cinemas,showTimes & studioShowTime

--- test/Cinemas.t.sol
+++ test/Cinemas.t.sol
@@ -4,33 +4,65 @@ pragma solidity ^0.8.13;
 import "../lib/forge-std/src/Test.sol";
 import "../lib/forge-std/src/console.sol";
 import "../src/Cinema.sol";
+import "../src/Ticket.sol";
 
 contract CinemasTest is Test {
     Cinema public cinemaContract;
-    bytes32[] public _names;
-    uint256[] public _studiosAmounts;
-    uint256[] public _studiosCapacities;
-
-    function cinemasToAdd() public {
-        for (uint256 i; i < 3; ++i) {
-            _names.push(keccak256(abi.encode("Ice Age ", i)));
-            _studiosAmounts.push(3);
-            _studiosCapacities.push(30 * (i + 1));
-        }
-    }
+    Ticket public ticketContract;
 
     function setUp() public {
         cinemaContract = new Cinema();
+        ticketContract = new Ticket();
+        ticketContract.setInterfaces(address(cinemaContract));
     }
 
-    function testAddCinemas() public {
-        cinemasToAdd();
-        cinemaContract.addCinemas(
-            10,
-            _names,
-            _studiosAmounts,
-            _studiosCapacities
+    function testAddRegion() public {
+        bytes32 name = keccak256("Cilegon");
+        cinemaContract.addRegionDetails(1, name, 5);
+    }
+
+    function testAddCinema() public {
+        bytes32 _name = keccak256(abi.encode("Cilegon"));
+        uint256 _studioAmount = 3;
+        uint256[] memory _studiosCapacities = new uint256[](3);
+        _studiosCapacities[0] = uint256(60);
+        _studiosCapacities[1] = uint256(70);
+        _studiosCapacities[2] = uint256(80);
+        cinemaContract.addCinema(10, _name, _studioAmount, _studiosCapacities);
+        (bytes32 name, uint256 studiosAmount, , , , , ) = cinemaContract
+            .getCinemaDetails(1);
+        console.log(studiosAmount);
+    }
+
+    function testAddShowTimes() public {
+        uint256[] memory times = new uint256[](4);
+
+        times[0] = uint256(10 hours);
+        times[1] = uint256(12 hours);
+        times[2] = uint256(16 hours);
+        times[3] = uint256(18 hours);
+
+        cinemaContract.adddShowTimes(1, times);
+        uint256[] memory timesAfter = cinemaContract.getCinemaShowTimes(1);
+        console.log(timesAfter[0]);
+        console.log(timesAfter[1]);
+        console.log(timesAfter[2]);
+    }
+
+    function testPairedStudiosWithShowTimes() public {
+        uint256[] memory studios = new uint256[](3);
+        studios[0] = uint256(0);
+        studios[1] = uint256(1);
+        studios[2] = uint256(2);
+        uint256[][] memory showTimes = new uint256[][](3);
+        showTimes[0] = studios;
+        showTimes[1] = studios;
+        showTimes[2] = studios;
+        cinemaContract.updateStudiosShowTimes(1, studios, showTimes);
+        uint256[] memory showTimesAfter = cinemaContract.getStudioShowTimes(
+            1,
+            0
         );
-        console.log(cinemaContract.getCinemaDetails(0).studiosAmount);
+        console.log(showTimesAfter[1]);
     }
 }
