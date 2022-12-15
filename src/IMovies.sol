333f911465dc9e470b585988ed1a14714da51eaf
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-15 20:05:57 +0700

    Removed isMoviesExists

--- src/IMovies.sol
+++ src/IMovies.sol
@@ -3,9 +3,4 @@ pragma solidity ^0.8.13;
 
 interface IMovies {
     function isMovieExist(uint256 _movie) external view returns (bool result);
-
-    function isMoviesExists(uint256[] calldata _movies)
-        external
-        view
-        returns (bool[] memory results);
 }
