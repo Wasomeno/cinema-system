6b38d1ba0d41ac100c4ae58896ce060089aab537
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-16 21:29:39 +0700

    Added addNewTransaction function

--- src/ITransactions.sol
+++ src/ITransactions.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-interface ITransactions {}
+interface ITransactions {
+    function addNewTransaction(
+        uint256 _region,
+        uint256 _cinema,
+        bytes32[] calldata _ticketIds,
+        uint256 _priceTotal
+    ) external;
+}
