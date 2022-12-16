bc62590d65b8dba2a594344e7564650ddadcf025
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-16 21:13:07 +0700

    Added isSuperAdmin

--- src/IRoles.sol
+++ src/IRoles.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.8.13;
 
 interface IRoles {
-    function checkAdmin(
+    function isCinemaAdmin(
         uint256 _region,
         uint256 _cinema,
         address _admin
     ) external view returns (bool result);
+
+    function isSuperAdmin() external view returns (bool result);
 }
