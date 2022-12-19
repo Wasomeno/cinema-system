132f5618ba9861efd81e80402e16a8d2b158c667
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-19 19:49:10 +0700

    Added getRegionsDetails, checkCinemaInRegion, addCinemasInRegion function

--- src/IRegion.sol
+++ src/IRegion.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-interface IRegion {}
+interface IRegion {
+    function getRegionsDetails(uint256 _region)
+        external
+        view
+        returns (
+            bytes32 _name,
+            uint256 _cinemasAmount,
+            uint256[] memory _cinemas
+        );
+
+    function checkCinemaInRegion(uint256 _region, uint256 _cinema)
+        external
+        view
+        returns (bool result);
+
+    function addCinemasInRegion(uint256 _region, uint256[] calldata _cinemas)
+        external;
+}
