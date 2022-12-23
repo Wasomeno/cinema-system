4a606ba9048b0a92035ff8a8ce51cdb9b75396aa
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-23 16:31:18 +0700

    Added new test functions (update & delete moveis)

--- test/Movies.t.sol
+++ test/Movies.t.sol
@@ -1,45 +1,60 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
 
 import "../lib/forge-std/src/Test.sol";
 import "../lib/forge-std/src/console.sol";
 import "../src/Movies.sol";
-import "../src/Cinema.sol";
 
 contract MoviesTest is Test {
-    Movies public moviesContract;
-    Cinema public cinemaContract;
-    bytes32[] public _titles;
-    uint256[] public _durations;
-    uint256[] public _studios;
+    Movies moviesContract;
 
-    function moviesToAddSetup() public {
-        for (uint256 i; i < 3; ++i) {
-            _titles.push(keccak256("Ice Age"));
-            _durations.push(2);
-            _studios.push(i + 1);
-        }
-    }
+    uint256 DEFAULT_MOVIE_ID = 1;
+    string DEFAULT_MOVIE_TITLE = "Movie";
+    uint256 DEFAULT_MOVIE_DURATION = 1200;
 
     function setUp() public {
         moviesContract = new Movies();
-        cinemaContract = new Cinema();
-        moviesContract.setInterfaces(address(cinemaContract));
+    }
+
+    function generateMovies()
+        internal
+        view
+        returns (
+            uint256[] memory ids,
+            bytes32[] memory titles,
+            uint256[] memory durations
+        )
+    {
+        uint256 id = DEFAULT_MOVIE_ID;
+        string memory title = DEFAULT_MOVIE_TITLE;
+        uint256 duration = DEFAULT_MOVIE_DURATION;
+        ids = new uint256[](5);
+        titles = new bytes32[](5);
+        durations = new uint256[](5);
+        for (uint256 i; i < 5; ++i) {
+            titles[i] = keccak256(abi.encode(title, id));
+            durations[i] = duration;
+            duration += 600;
+            id += 1;
+        }
     }
 
     function testAddMovies() public {
-        moviesToAddSetup();
-        moviesContract.addMovies(_titles, _durations, _studios);
-        console.log(moviesContract.getMovieDetails(1).duration);
+        (
+            uint256[] memory movieIds,
+            bytes32[] memory titles,
+            uint256[] memory durations
+        ) = generateMovies();
+        moviesContract.addMovies(movieIds, titles, durations);
     }
 
-    function testAddMoviesToCinema() public {
-        uint256[] memory moviesToAdd = new uint256[](3);
-        for (uint256 i; i < 3; ++i) {
-            moviesToAdd[i] = i + 1;
-        }
-        moviesContract.addMoviesToCinema(123, moviesToAdd);
-        uint32[] memory movies = moviesContract.getMoviesInCinema(123);
-        console.log(uint256(movies[0]));
+    function testDeleteMovies() public {
+        (
+            uint256[] memory movieIds,
+            bytes32[] memory titles,
+            uint256[] memory durations
+        ) = generateMovies();
+        moviesContract.addMovies(movieIds, titles, durations);
+        moviesContract.deleteMovies(movieIds);
     }
 }
